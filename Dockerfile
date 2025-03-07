2cbdd57922a8ef2b7059cea864049c43e67ce80b
Bo YU <tsu.yubo@gmail.com>
2025-03-07 19:37:20 +0800

    initial commit
    Signed-off-by: Bo YU <tsu.yubo@gmail.com>

---
+++ Dockerfile
@@ -0,0 +1,48 @@
+FROM debian:sid as builder
+MAINTAINER Bo YU "tsu.yubo@gmail.com"
+
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN --mount=type=cache,sharing=shared,target=/var/cache \
+    --mount=type=cache,sharing=shared,target=/var/lib/apt/lists \
+    --mount=type=tmpfs,target=/usr/share/man \
+    --mount=type=tmpfs,target=/usr/share/doc \
+    apt-get update \
+    && apt-get install -y eatmydata \
+    && eatmydata apt-get install -y debootstrap qemu-user-static \
+        binfmt-support gdisk kpartx \
+        parted \
+        autoconf automake autotools-dev bc binfmt-support \
+        build-essential cpio curl \
+        dosfstools e2fsprogs fdisk flex gawk  \
+        git gperf kmod libexpat-dev \
+        libgmp-dev libmpc-dev libmpfr-dev libssl-dev \
+        libtool mmdebstrap multistrap openssl parted \
+        patchutils python3 python3-dev \
+        python3-setuptools qemu-user-static swig \
+        systemd-container texinfo zlib1g-dev wget
+
+# build rootfs 
+FROM builder as build_rootfs
+WORKDIR /build
+COPY rootfs/build.sh build.sh
+#COPY rootfs/addon addon
+#COPY rootfs
+
+RUN --mount=type=cache,sharing=shared,target=/var/cache \
+    --mount=type=cache,sharing=shared,target=/var/lib/apt/lists \
+    --mount=type=tmpfs,target=/usr/share/man \
+    --mount=type=tmpfs,target=/usr/share/doc \
+    eatmydata multistrap -f multistrap.conf
+
+
+FROM builder as build_image
+WORKDIR /builder
+COPY --from=build_rootfs /port/rv64-port/ ./rv64-port/
+COPY create_image.sh build.sh ./
+COPY rootfs/setup_rootfs.sh ./rv64-port/
+RUN ls -l ./rv64-port/usr/lib/u-boot/sifive_unmatched
+
+CMD eatmydata /builder/build.sh
+
+
---
+++ scripts/make_rootfs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+make_rootfs_tarball()
+{
+    # use $1
+    #PACKAGE_LIST="$KEYRINGS $GPU_DRIVER $BASE_TOOLS $GRAPHIC_TOOLS $XFCE_DESKTOP $BENCHMARK_TOOLS $FONTS $INCLUDE_APPS $EXTRA_TOOLS $LIBREOFFICE"
+    PACKAGE_LIST="$KEYRINGS $BASE_TOOLS $BENCHMARK_TOOLS $EXTRA_TOOLS"
+    mmdebstrap --architectures=riscv64 \
+        --include="$PACKAGE_LIST" \
+        sid $1 \
+        "deb https://deb.debian.org/debian sid main contrib non-free non-free-firmware"
+}
+
+make_rootfs()
+{
+    if [[ -z "$USE_TARBALL" ]]; then
+        echo "env USE_TARBALL is set to the empty string!"
+        echo "create rootfs"
+        make_rootfs_tarball $CHROOT_TARGET
+    else
+        tar xpvf $USE_TARBALL --xattrs-include='*.*' --numeric-owner -C $CHROOT_TARGET
+    fi
+
+    # move /boot contents to other place
+    if [ ! -z "$(ls -A "$CHROOT_TARGET"/boot/)" ]; then
+        mkdir "$CHROOT_TARGET"/mnt/boot
+        mv -v "$CHROOT_TARGET"/boot/* "$CHROOT_TARGET"/mnt/boot/
+    fi
+
+    # Mount chroot path
+    mount "$BOOT_IMG" "$CHROOT_TARGET"/boot
+    mount -t proc /proc "$CHROOT_TARGET"/proc
+    mount -B /sys "$CHROOT_TARGET"/sys
+    mount -B /run "$CHROOT_TARGET"/run
+    mount -B /dev "$CHROOT_TARGET"/dev
+    mount -B /dev/pts "$CHROOT_TARGET"/dev/pts
+    mount -t tmpfs tmpfs "$CHROOT_TARGET"/tmp
+    mount -t tmpfs tmpfs "$CHROOT_TARGET"/var/tmp
+    mount -t tmpfs tmpfs "$CHROOT_TARGET"/var/cache/apt/archives/
+
+    # move boot contents back to /boot
+    if [ ! -z "$(ls -A "$CHROOT_TARGET"/mnt/boot/)" ]; then
+        mv -v "$CHROOT_TARGET"/mnt/boot/* "$CHROOT_TARGET"/boot/
+        rmdir "$CHROOT_TARGET"/mnt/boot
+    fi
+
+    # apt update
+    chroot "$CHROOT_TARGET" sh -c "apt update"
+}
